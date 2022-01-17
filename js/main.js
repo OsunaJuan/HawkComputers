478320cf6b0862201558126ccfb428cd0d8bf005
juan <osunajuan15@gmail.com>
2022-01-17 20:10:05 -0300

    cambios menores

--- js/main.js
+++ js/main.js
@@ -108,20 +108,19 @@ var carrito = [];
 // Se añaden los productos a vender en la pagina
 
     //Listado de Cafes.
-insertarProducto(ListadoCafes,1,"Cafe Brasil Catuai",500,25,"Cafe");
-insertarProducto(ListadoCafes,2,"Cafe Colombia Guanes",500,25,"Cafe");
-insertarProducto(ListadoCafes,3,"Cafe Blend Brasil Colombia",500,25,"Cafe");
-insertarProducto(ListadoCafes,4,"Cafe Brasil Bourbon",500, 25, "Cafe");
-insertarProducto(ListadoCafes,5,"Cafe Guatemala",500,25,"Cafe");
-insertarProducto(ListadoCafes,6,"Cafe Etiopia", 500,25,"Cafe");
+insertarProducto(ListadoCafes,1,"Cafe Brasil Catuai",550,25,"Cafe");
+insertarProducto(ListadoCafes,2,"Cafe Colombia Guanes",630,25,"Cafe");
+insertarProducto(ListadoCafes,3,"Cafe Blend Brasil Colombia",450,25,"Cafe");
+insertarProducto(ListadoCafes,4,"Cafe Brasil Bourbon",520, 25, "Cafe");
+insertarProducto(ListadoCafes,5,"Cafe Guatemala",650,25,"Cafe");
+insertarProducto(ListadoCafes,6,"Cafe Etiopia", 700,25,"Cafe");
 
     //Listado de Capsulas
-
-insertarProducto(ListadoCapsulas,7,"Capsulas de Cafe de especialidad Brasil ",650,25,"Capsulas");
-insertarProducto(ListadoCapsulas,8,"Capsulas de Cafe de especialidad Colombia ",650,25,"Capsulas");
-insertarProducto(ListadoCapsulas,9,"Capsulas de Cafe de especialidad Oreiras ",650,25,"Capsulas");
+insertarProducto(ListadoCapsulas,7,"Capsulas de Cafe de especialidad Brasil ",600,25,"Capsulas");
+insertarProducto(ListadoCapsulas,8,"Capsulas de Cafe de especialidad Colombia ",590,25,"Capsulas");
+insertarProducto(ListadoCapsulas,9,"Capsulas de Cafe de especialidad Oreiras ",620,25,"Capsulas");
 insertarProducto(ListadoCapsulas,10,"Capsulas de Cafe Caffetino Dolce ",650,25,"Capsulas");
-insertarProducto(ListadoCapsulas,11,"Capsulas de Cafe Caffetino Nespresso ",650,25,"Capsulas");
+insertarProducto(ListadoCapsulas,11,"Capsulas de Cafe Caffetino Nespresso ",610,25,"Capsulas");
 
 //Tienda de Cafes
 
@@ -137,7 +136,9 @@ do{
         categoriasTienda = Number(prompt("Selecciona una opcion:\n"+
         "1) ver todos los productos\n"+
         "2) Para ver solo los cafes\n" + 
-        "3) Para ver solo las capsulas de cafe"));
+        "3) Para ver solo las capsulas de cafe\n" +
+        "4) Para ver los productos de menor precio a mayor precio\n" + 
+        "5) Para ver los productos de mayor a menor precio"));
 
         let compra;
     
@@ -149,6 +150,19 @@ do{
 
         }else if(categoriasTienda == 3){
             displayTienda(ListadoCapsulas, carrito);
+        }else if(categoriasTienda == 4){
+
+            let leastToMost = totalProductos;
+            leastToMost.sort(function(a,b){return a.value - b.value});
+            displayTienda(leastToMost,carrito);
+            
+            
+        }else if(categoriasTienda == 5){
+
+            let mostToLeast = totalProductos;
+            mostToLeast.sort(function(a,b){return b.value - a.value});
+            displayTienda(mostToLeast,carrito);
+
         }else{
             alert("Has introduccido una opcion incorrecta!")
         }
